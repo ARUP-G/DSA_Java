87040fde1c7a02495a9e1eeb5379ce93fb90fab9
Arup <darup2019@gmail.com>
2023-06-19 21:47:04 +0530

    Recursive char program added

---
+++ String/Str_Q/RecursiveChar.java
@@ -0,0 +1,28 @@
+package DSA.String.Str_Q;
+
+public class RecursiveChar {
+    public static void main(String[] args) {
+        String in = "aabbc";
+        System.out.println(removeConsecutiveCharacter(in));
+
+    }
+
+    static String removeConsecutiveCharacter(String S) {
+
+        String snew = S.toLowerCase();
+        StringBuilder sb = new StringBuilder(snew);
+        int i = 1;
+        int j = i - 1;
+
+        while (i < sb.length()) {
+            if (sb.charAt(i) == sb.charAt(j)) {
+                sb.deleteCharAt(i);
+                //snew = sb.toString();
+            }
+            i++;
+            j++;
+        }
+        return sb.toString();
+    }
+
+}
