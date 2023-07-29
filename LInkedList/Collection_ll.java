929f284d657034ddd2f83b7a374ed057075d55f0
Arup <darup2019@gmail.com>
2023-07-29 23:42:24 +0530

    LinkedList using collections added

---
+++ LInkedList/Collection_ll.java
@@ -0,0 +1,47 @@
+package DSA_Java.LinkedList;
+
+import java.util.LinkedList;
+public class Collection_ll {
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<>();
+
+        // Add
+        list.add("A");
+        list.add("B");
+        list.add("C");
+
+        // Add by index
+        list.add(1,"F");
+
+        // Print
+        System.out.println("List: "+list);
+
+        // Retrieve
+        // By index
+        System.out.println("Element at index 1: "+list.get(1));
+
+        // By position
+        System.out.println("First element: "+list.getFirst());
+        System.out.println("Last element: "+list.getLast());
+
+        // Shows head by default
+        System.out.println("Peek: "+list.peek());
+        // Shows head
+        System.out.println("Peek first:"+list.peekFirst());
+        // Shows tail
+        System.out.println("Peek Last:"+list.peekLast());
+
+
+        // Delete
+//        list.remove("F");
+
+        System.out.println(list);
+
+
+
+        // Check
+        System.out.println(list.contains("A"));
+
+
+    }
+}
