dd49da9f1e520fe9ecaeb1b78c609fe085cc8404
Arup <darup2019@gmail.com>
2023-07-30 22:23:21 +0530

    Array Question SumofSubarray and Search_rotatedArray added

---
+++ Array/Ques/Search_rotatedArray.java
@@ -0,0 +1,78 @@
+package DSA_Java.Array.Ques;
+// There is an integer array nums sorted in ascending order (with distinct values).
+// Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length)
+// such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed).
+// For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+
+// Given the array nums after the possible rotation and an integer target,
+// return the index of target if it is in nums, or -1 if it is not in nums.
+public class Search_rotatedArray {
+    public static void main(String[] args) {
+        int[] arr ={1,3,4,5,7,8,9};
+        int n = arr.length;
+        int k = 2; // Rotation till
+        System.out.println("Original Array: ");
+        printArr(arr);
+
+        System.out.println("After rotation: ");
+        leftRotate(arr,k,n);
+
+        printArr(arr);
+        int res= search(arr,1);
+        System.out.println("Index of target: "+res);
+
+    }
+    static void leftRotate(int[] arr, int k, int n){
+        // make a temp array
+        int[] temp = new int[k];
+
+        for (int i=0; i<k; i++){
+            temp[i] = arr[i];
+        }
+
+        //remaining array
+        for(int i=k;i<n;i++){
+            arr[i-k] = arr[i];
+        }
+        // adding temp array to main array
+        for (int i = 0; i < k; i++) {
+            arr[i+n-k] = temp[i];
+        }
+    }
+    static int search(int[] nums, int target) {
+        int n =nums.length;
+        int left = 0;
+        int right = n-1;
+        int mid;
+
+        while(left<=right)
+        {
+            mid = (left+right)/2;
+
+            if(nums[mid]==target)
+                return mid;
+
+            else if(nums[mid]>=nums[left])
+            {
+                if(target<=nums[mid] && target>=nums[left])
+                    right = mid-1;
+                else
+                    left = mid+1;
+            }
+            else
+            {
+                if(target>=nums[mid]&& target<=nums[right])
+                    left = mid+1;
+                else
+                    right = mid-1;
+            }
+        }
+        return -1;
+    }
+    static void printArr(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+ " ");
+        }
+        System.out.println();
+    }
+}
