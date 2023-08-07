fcf7f9f63693fa670eb583632c13adeddd0e2547
Arup <darup2019@gmail.com>
2023-08-07 22:40:06 +0530

    Case check file aded to random

---
+++ random/CaseCheck.java
@@ -0,0 +1,15 @@
+package DSA_Java.random;
+
+import java.util.Scanner;
+
+public class CaseCheck {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        char ch = input.next().trim().charAt(0);
+        char cc = input.next().trim().charAt(2);
+
+        System.out.println(ch);
+        System.out.println(cc);
+
+    }
+}
